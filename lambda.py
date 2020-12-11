f4ad3123d49715269d81e93d1054cc098493a738
root <root@ip-10-0-0-19.ap-southeast-2.compute.internal>
2020-12-11 02:22:51 +0000

    lambda update

--- lambda.py
+++ lambda.py
@@ -10,6 +10,8 @@
 # OF ANY KIND, either express or implied. See the License for the specific
 # language governing permissions and limitations under the License.
 
+#Add a note by Kamran
+
 import os
 import boto3
 import email
